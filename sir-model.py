b107bd5646e2249661fb8d2af0c2b3c3399b1caf
jmrico01 <jrico@princeton.edu>
2017-12-12 20:27:50 -0500

    Changed output of SIR model to cumulative # of new cases

--- sir-model.py
+++ sir-model.py
@@ -2,38 +2,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button, RadioButtons
 
-# SIR model
-#   dS/dt = -beta S I
-#   dI/dt = beta S I - gamma I
-#   dR/dt = gamma I
-def Step(S, I, R, beta, gamma):
-    dS = -beta * S * I
-    dI = beta * S * I - gamma * I
-    dR = gamma * I
-    return (S + dS, I + dI, R + dR)
-
 def RunSIR(startS, startI, startR, beta, gamma, T):
-    S = [startS] * T
-    I = [startI] * T
-    R = [startR] * T
+    S = [0] * T
+    I = [0] * T
+    R = [0] * T
+    output = [0] * T
+    S[0] = startS
+    I[0] = startI
+    R[0] = startR
     for t in range(1, T):
-        S[t], I[t], R[t] = Step(S[t-1], I[t-1], R[t-1], beta, gamma)
+        dS = -beta * S[t-1] * I[t-1]
+        dI = beta * S[t-1] * I[t-1] - gamma * I[t-1]
+        dR = gamma * I[t-1]
+        # output: we're only recording the number of new cases
+        dOutput = beta * S[t-1] * I[t-1]
+
+        S[t] = S[t-1] + dS
+        I[t] = I[t-1] + dI
+        R[t] = R[t-1] + dR
+        output[t] = output[t-1] + dOutput
     
-    return S, I, R
+    return [S, I, R, output]
 
-T = 10000
+T = 100000
 
-startS = 12e6
-startI = 1800
+startS = 7e6
+startI = 935
 startR = 0
 beta = 3.2e-10
 betaRange = 0.5
 r0Min = 1.0
-r0Max = 4.0
-r0 = (r0Min + r0Max) / 2.0
+r0Max = 1.1
+r0 = 1.02
 gamma = beta * startS / r0
 
-S, I, R = RunSIR(startS, startI, startR, beta, gamma, T)
+[S, I, R, output] = RunSIR(startS, startI, startR, beta, gamma, T)
 
 fig, ax = plt.subplots()
 plt.subplots_adjust(left = 0.25, bottom = 0.25)
@@ -41,6 +44,7 @@ t = np.linspace(0.0, 1.0, num = T, endpoint = True)
 lineS, = plt.plot(t, S, lw=1.5, color='yellow')
 lineI, = plt.plot(t, I, lw=1.5, color='red')
 lineR, = plt.plot(t, R, lw=1.5, color='blue')
+#lineOut, = plt.plot(t, output, color="red")
 #plt.axis([0, 1, -10, 10])
 
 axisColor = 'lightgoldenrodyellow'
@@ -57,10 +61,11 @@ def UpdateSIR(val):
     beta = sliderBeta.val
     r0 = sliderR0.val
     gamma = beta * startS / r0
-    S, I, R = RunSIR(startS, startI, startR, beta, gamma, T)
+    [S, I, R, output] = RunSIR(startS, startI, startR, beta, gamma, T)
     lineS.set_ydata(S)
     lineI.set_ydata(I)
     lineR.set_ydata(R)
+    #lineOut.set_ydata(output)
     fig.canvas.draw_idle()
 
 sliderBeta.on_changed(UpdateSIR)
